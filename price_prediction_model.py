1eaa79017bf20fe3dabf9971a91ccb4d4769a9c4
Mark Lerret <150942545+markalerret@users.noreply.github.com>
2024-03-25 20:09:51 -0400

    Commit completed file

--- price_prediction_model.py
+++ price_prediction_model.py
@@ -16,23 +16,23 @@ file = 'housing.csv'
 # Fill in the line below to read the file into a variable data
 data = pd.read_csv(file)
 
-print(data.columns)
+#print(data.columns)
 
 # Print summary statistics in next line
-print(data.describe())
-print(data.bedrooms.describe())
-print(data.bathrooms.describe())
+#print(data.describe())
+#print(data.bedrooms.describe())
+#print(data.bathrooms.describe())
 
 
 # What is the average square footage (rounded to nearest integer)?
 avg_sq_ft = data.area.mean()
 #5151 square feet
-print(int(avg_sq_ft.round()))
+#print(int(avg_sq_ft.round()))
 
 # What is the average number of bedrooms (rounded to nearest integer)?
 avg_bedrooms = data.bedrooms.mean()
 #3
-print(int(avg_bedrooms.round()))
+#print(int(avg_bedrooms.round()))
 
 """Desicion Tree Machine Learning Model"""
 # Set prediction target
@@ -43,8 +43,8 @@ features = ['area', 'bedrooms', 'bathrooms', 'stories']
 X = data[features]
 
 # Look at numerical features
-print(X.describe())
-print(X.head)
+#print(X.describe())
+#print(X.head)
 
 # Define model. Specify a number for random_state to ensure same results each run
 housing_model = DecisionTreeRegressor(random_state=1)
@@ -53,16 +53,16 @@ housing_model = DecisionTreeRegressor(random_state=1)
 housing_model.fit(X, y)
 
 # Test the prediction process, we will do a more appropraite test later
-print("Making predictions for the following 5 houses:")
-print(X.head())
-print("The predictions are")
-print(housing_model.predict(X.head()))
+#print("Making predictions for the following 5 houses:")
+#print(X.head())
+#print("The predictions are")
+#print(housing_model.predict(X.head()))
 # Actual prices
-print(y[:5])
+#print(y[:5])
 
 # Mean absolute error = 145,165
 predicted_home_prices = housing_model.predict(X)
-print(mean_absolute_error(y, predicted_home_prices))
+#print(mean_absolute_error(y, predicted_home_prices))
 
 
 """Validate Model"""
@@ -79,7 +79,7 @@ housing_model.fit(train_X, train_y)
 # get predicted prices on validation data
 val_predictions = housing_model.predict(val_X)
 # Mean absolute error is approx. 1,300,000
-print(mean_absolute_error(val_y, val_predictions))
+#print(mean_absolute_error(val_y, val_predictions))
 
 
 """Underfitting/Overfitting"""
@@ -96,14 +96,63 @@ candidate_max_leaf_nodes = [5, 25, 50, 100, 250, 500]
 maes = []
 for max in candidate_max_leaf_nodes:
     maes.append(get_mae(max, train_X, val_X, train_y, val_y))
-print(maes)    
+#print(maes)    
 
 # Store the best value of max_leaf_nodes (it will be either 5, 25, 50, 100, 250 or 500)
 best_tree_size = candidate_max_leaf_nodes[maes.index(min(maes))]
-print(best_tree_size)
+#print(best_tree_size)
 
 # Fill in argument to make optimal size and uncomment
 final_model = DecisionTreeRegressor(max_leaf_nodes = best_tree_size, random_state=2)
 
 # fit the final model and uncomment the next two lines
 final_model.fit(X, y)
+
+
+"""Random Forest"""
+from sklearn.ensemble import RandomForestRegressor
+
+# Define the model. Set random_state to 1
+rf_model = RandomForestRegressor(random_state = 1)
+
+# fit your model
+rf_model.fit(train_X, train_y)
+
+# Calculate the mean absolute error of your Random Forest model on the validation data
+val_predictions = rf_model.predict(val_X)
+rf_val_mae = mean_absolute_error(val_y, val_predictions)
+
+#print("Validation MAE for Random Forest Model: {}".format(rf_val_mae))
+
+"""Create a model for Competition Submission"""
+# Select columns corresponding to features, and preview the data
+X = data[features]
+X.head()
+
+# Split into validation and training data
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1)
+
+# Define a random forest model
+rf_model = RandomForestRegressor(random_state=1)
+rf_model.fit(train_X, train_y)
+rf_val_predictions = rf_model.predict(val_X)
+rf_val_mae = mean_absolute_error(rf_val_predictions, val_y)
+
+#print("Validation MAE for Random Forest Model: {:,.0f}".format(rf_val_mae))
+
+# To improve accuracy, create a new Random Forest model which you will train on all training data
+rf_model_on_full_data = RandomForestRegressor(random_state=2)
+
+# fit rf_model_on_full_data on all data from the training data
+rf_model_on_full_data.fit(X, y)
+rf_model_on_full_data_val_predictions = rf_model_on_full_data.predict(val_X)
+rf_model_on_full_data_val_mae = mean_absolute_error(rf_model_on_full_data_val_predictions, val_y)
+
+# create test_X which comes from test_data but includes only the columns you used for prediction.
+# The list of columns is stored in a variable called features
+test_X = data[features]
+
+# make predictions which we will submit. 
+test_preds = rf_model_on_full_data.predict(test_X)
+
+#print(test_preds)
